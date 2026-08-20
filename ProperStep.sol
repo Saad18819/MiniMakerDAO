156e67a3510b889e559b975d69373d39257442a5
Saad18819 <eucalyptuszenthura1234@gmail.com>
2026-08-20 07:28:55 +0000

    Initial commit

---
+++ ProperStep.sol
@@ -0,0 +1,14 @@
+/*
+
+1.forge init
+2.forge install smartcontractkit/chainlink-brownie-contracts --no-commit
+
+// This command downloads external Smart Contracts (specifically Chainlink's interfaces, like price feeds) into your Foundry project so you can import them directly into your Solidity code.
+
+3. forge install openzeppelin/openzeppelin-contracts --no-commit
+
+4.update remapping in foundry.toml
+
+5.
+
+ */
