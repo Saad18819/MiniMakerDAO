74493a1e6324ac9ebfbe96b36ad2e397166a48e5
Saad18819 <eucalyptuszenthura1234@gmail.com>
2026-09-21 13:32:58 +0530

    PROJECT

--- ProperStep.sol
+++ ProperStep.sol
@@ -5,6 +5,8 @@
 3.forge install openzeppelin/openzeppelin-contracts
 4.update remapping in foundry.toml
 
-5.
+5.and then write dEngineToken code basically its a erc token which i havent studied so chagpt it and go through the code and try to understand as much as i can
+6. write vault engine code and then script and then did testing
+7.make sure to write README and upload on github
 
  */
